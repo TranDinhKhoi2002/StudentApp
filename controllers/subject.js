2765499976a1b6323df33bd1c149040c225a3a62
nghia0111 <20521659@gm.uit.edu.vn>
2022-10-28 14:58:20 +0700

    add controller for subject and teacher

--- controllers/subject.js
+++ controllers/subject.js
@@ -1,6 +1,33 @@
 const { validationResult } = require("express-validator");
 const Subject = require("../models/subject");
-const Teacher = require("../models/teacher")
+const Teacher = require("../models/teacher");
+const { checkPrincipalRole } = require("../util/checkStaffAndPrincipalRole");
+
+exports.getSubjects = async (req, res, next) => {
+  try {
+    const subjects = await Subject.find().populate("teachers", "name");
+    res.status(200).json({ subjects: subjects });
+  } catch (err) {
+    const error = new Error("Có lỗi xảy ra, vui lòng thử lại sau");
+    error.statusCode = 500;
+    next(error);
+  }
+};
+
+exports.getSubject = async (req, res, next) => {
+  const subjectId = req.params.subjectId;
+  try {
+    const subject = await Subject.findById(subjectId).populate(
+      "teachers",
+      "name"
+    );
+    res.status(200).json({ subject: subject });
+  } catch (err) {
+    const error = new Error("Có lỗi xảy ra, vui lòng thử lại sau");
+    error.statusCode = 500;
+    next(error);
+  }
+};
 
 exports.createSubject = async (req, res, next) => {
   const errors = validationResult(req);
@@ -14,9 +41,15 @@ exports.createSubject = async (req, res, next) => {
   const { name, passScore } = req.body;
 
   try {
+    const isAuthorized = await checkPrincipalRole(req.accountId);
+    if (!isAuthorized) {
+      const error = new Error("Chỉ có hiệu trưởng mới được thêm môn học");
+      error.statusCode = 401;
+      return next(error);
+    }
     const subject = new Subject({
       name,
-      passScore
+      passScore,
     });
     await subject.save();
     res.status(201).json({ message: "Thêm môn học thành công" });
@@ -39,6 +72,12 @@ exports.updateSubject = async (req, res, next) => {
   const subjectId = req.params.subjectId;
   const { name, passScore } = req.body;
   try {
+    const isAuthorized = await checkPrincipalRole(req.accountId);
+    if (!isAuthorized) {
+      const error = new Error("Chỉ có hiệu trưởng mới được chỉnh sửa môn học");
+      error.statusCode = 401;
+      return next(error);
+    }
     const subject = await Subject.findById(subjectId);
     if (!subject) {
       const error = new Error("Môn học không tồn tại");
@@ -46,7 +85,7 @@ exports.updateSubject = async (req, res, next) => {
       return next(error);
     }
     subject.name = name;
-    subject.passScore =passScore;
+    subject.passScore = passScore;
     await subject.save();
     res.status(200).json({ message: "Cập nhật môn học thành công" });
   } catch (err) {
@@ -57,25 +96,34 @@ exports.updateSubject = async (req, res, next) => {
 };
 
 exports.deleteSubject = async (req, res, next) => {
-    const subjectId = req.params.subjectId;
-    try {
-      const subject = await Subject.findById(subjectId);
-      if (!subject) {
-        const error = new Error("Môn học không tồn tại");
-        error.statusCode = 404;
-        return next(error);
-      }
-      const teachers = await Teacher.findOne({subject: subjectId})
-      if(teachers){
-        res.status(400).json({ message: "Môn học vẫn đang được giảng dạy, vui lòng chỉnh sửa thông tin giáo viên trước khi xóa môn học" });
-      }
-      else{
-        await Subject.findByIdAndRemove(subjectId);
-        res.status(200).json({ message: "Xóa môn học thành công" });
-      }
-    } catch (err) {
-      const error = new Error("Có lỗi xảy ra, vui lòng thử lại sau");
-      error.statusCode = 500;
-      next(error);
+  const subjectId = req.params.subjectId;
+  try {
+    const isAuthorized = await checkPrincipalRole(req.accountId);
+    if (!isAuthorized) {
+      const error = new Error("Chỉ có hiệu trưởng mới được xóa môn học");
+      error.statusCode = 401;
+      return next(error);
     }
-  };
+    const subject = await Subject.findById(subjectId);
+    if (!subject) {
+      const error = new Error("Môn học không tồn tại");
+      error.statusCode = 404;
+      return next(error);
+    }
+    const existingTeacher = await Teacher.findOne({ subject: subjectId });
+    if (existingTeacher) {
+      const error = new Error(
+        "Môn học vẫn đang được giảng dạy, vui lòng chỉnh sửa thông tin giáo viên trước khi xóa"
+      );
+      error.statusCode = 422;
+      next(error);
+    } else {
+      await Subject.findByIdAndRemove(subjectId);
+      res.status(200).json({ message: "Xóa môn học thành công" });
+    }
+  } catch (err) {
+    const error = new Error("Có lỗi xảy ra, vui lòng thử lại sau");
+    error.statusCode = 500;
+    next(error);
+  }
+};
