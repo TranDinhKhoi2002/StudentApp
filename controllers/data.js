d64174b1eaaaac5bd23ec7b6eaa9c0f00af6f42e
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-10-29 10:43:01 +0700

    fix data route

--- controllers/data.js
+++ controllers/data.js
@@ -5,17 +5,16 @@ exports.getData = async (req, res, next) => {
   const accountId = req.accountId;
   try {
     let classes = [];
-    const teacher = await Teacher.findOne({ account: accountId }).populate(
-      "classes",
-      "name"
-    );
+    const teacher = await Teacher.findOne({ account: accountId })
+      .populate("classes", "name")
+      .populate("role");
     if (teacher) {
       classes = teacher.classes;
     }
 
     const subjects = await Subject.find();
 
-    res.status(200).json({ classes, subjects });
+    res.status(200).json({ classes, subjects, role: teacher.role.name });
   } catch (err) {
     const error = new Error("Có lỗi xảy ra, vui lòng thử lại sau");
     error.statusCode = 500;
