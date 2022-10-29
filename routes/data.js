501374035c5d9d92117212b8a8321532d3ad235a
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-10-29 16:20:29 +0700

    fix

--- routes/data.js
+++ routes/data.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const dataController = require("../controllers/data");
 const isAuth = require("../middleware/is-auth");
 
-router.get("/data", dataController.getData);
+router.get("/data", isAuth, dataController.getData);
 
 module.exports = router;
