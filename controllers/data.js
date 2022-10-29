cb7eec5fcf292e5657f944a9f192f085dd21d582
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-10-29 16:24:49 +0700

    deploy

--- controllers/data.js
+++ controllers/data.js
@@ -6,7 +6,7 @@ exports.getData = async (req, res, next) => {
   try {
     let classes = [];
     const teacher = await Teacher.findOne({ account: accountId })
-      .populate("classes", "name")
+      .populate("classes")
       .populate("role");
     if (teacher) {
       classes = teacher.classes;
@@ -17,13 +17,11 @@ exports.getData = async (req, res, next) => {
     const classesName = classes.map((_class) => _class.name);
     const subjectsName = subjects.map((subject) => subject.name);
 
-    res
-      .status(200)
-      .json({
-        classes: classesName,
-        subjects: subjectsName,
-        role: teacher.role.name,
-      });
+    res.status(200).json({
+      classes: classesName,
+      subjects: subjectsName,
+      role: teacher.role.name,
+    });
   } catch (err) {
     const error = new Error("Có lỗi xảy ra, vui lòng thử lại sau");
     error.statusCode = 500;
